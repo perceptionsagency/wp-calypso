cc6066b256a4678474c1ebf103a577e66cc202cf
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-14 20:12:11 +0200

    Move receive status to happychat/connection

--- client/state/happychat/actions.js
+++ client/state/happychat/actions.js
@@ -3,12 +3,7 @@
 /**
  * Internal dependencies
  */
-import { HAPPYCHAT_BLUR, HAPPYCHAT_FOCUS, HAPPYCHAT_IO_RECEIVE_STATUS } from 'state/action-types';
-
-export const receiveStatus = status => ( {
-	type: HAPPYCHAT_IO_RECEIVE_STATUS,
-	status,
-} );
+import { HAPPYCHAT_BLUR, HAPPYCHAT_FOCUS } from 'state/action-types';
 
 export const blur = () => ( { type: HAPPYCHAT_BLUR } );
 export const focus = () => ( { type: HAPPYCHAT_FOCUS } );
